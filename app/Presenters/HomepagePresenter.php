03b9398442c4a502ee1e41bfc96f0aa11b013e97
tbreuss <thomasbreuss@gmx.ch>
2021-06-29 12:54:43 +0200

    Add sqlite database, retrieve latest blog posts

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -6,7 +6,19 @@ namespace App\Presenters;
 
 use Nette;
 
-
-final class HomepagePresenter extends Nette\Application\UI\Presenter
+class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    private Nette\Database\Explorer $database;
+
+    public function __construct(Nette\Database\Explorer $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->posts = $this->database->table('posts')
+            ->order('created_at DESC')
+            ->limit(5);
+    }
 }
