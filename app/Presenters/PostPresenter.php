f5b8190bde364677ccb0e6509212a8dc987fb27e
tbreuss <thomasbreuss@gmx.ch>
2021-06-30 10:41:43 +0200

    Create and edit post

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -60,4 +60,42 @@ class PostPresenter extends Nette\Application\UI\Presenter
         $this->redirect('this');
     }
 
+    protected function createComponentPostForm(): Form
+    {
+        $form = new Form;
+        $form->addText('title', 'Title:')
+            ->setRequired();
+        $form->addTextArea('content', 'Content:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Save and publish');
+        $form->onSuccess[] = [$this, 'postFormSucceeded'];
+
+        return $form;
+    }
+
+    public function postFormSucceeded(Form $form, array $values): void
+    {
+        $postId = $this->getParameter('postId');
+
+        if ($postId) {
+            $post = $this->database->table('posts')->get($postId);
+            $post->update($values);
+        } else {
+            $post = $this->database->table('posts')->insert($values);
+        }
+
+        $this->flashMessage('Post was published', 'success');
+        $this->redirect('show', $post->id);
+    }
+
+    public function actionEdit(int $postId): void
+    {
+        $post = $this->database->table('posts')->get($postId);
+        if (!$post) {
+            $this->error('Post not found');
+        }
+        $this['postForm']->setDefaults($post->toArray());
+    }
+
 }
